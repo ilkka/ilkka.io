2ceefee417a1de6b0ea87b50abf14d5bd97b01c3
Ilkka Laukkanen <ilkka@fastmail.fm>
2015-02-21 18:16:17 +0200

    Turn on compass sourcemaps
    Also make config usable from other dirs by setting project_path. Odd
    that it isn't inferred correctly.

--- themes/ilkka.io/config.rb
+++ themes/ilkka.io/config.rb
@@ -3,10 +3,12 @@ require 'compass/import-once/activate'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
+project_path = File.dirname(__FILE__)
 css_dir = "static/css"
 sass_dir = "sass"
 images_dir = "static/images"
 javascripts_dir = "static/js"
+sourcemap = true
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
